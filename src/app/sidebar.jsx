beba2eea75da1efccceb77b0190daebf41d9683e
MUHAMMAD RIFQI AZIZ <958632702@intranet.pajak.go.id>
2024-12-27 17:13:57 +0700

    jumat last

--- src/app/sidebar.jsx
+++ src/app/sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 import { Tabs  } from "@chakra-ui/react"
 import { LuUser,LuFolder,LuFileInput  } from 'react-icons/lu'
@@ -8,6 +8,9 @@ import Preprocess from '@/components/preprocess'
 
 
 const sidebar = () => {
+
+
+  const [valueTabs, setvalueTabs] = useState('input')
 
 
 
@@ -27,26 +30,26 @@ const sidebar = () => {
         </Button>
 
         <h2 className='subtitle'>Add Nodes</h2>
-        <Tabs.Root defaultValue="members" fitted variant="plain" rounded='lg'>
-          <Tabs.List bg="bg.muted" rounded="lg" p="2" >
-            <Tabs.Trigger value="members" >
+        <Tabs.Root colorPalette='gray' defaultValue="input" fitted variant="plain" rounded='lg'>
+          <Tabs.List bg='green.100' rounded="lg" p="2" >
+            <Tabs.Trigger value='input' _selected={{color:'white'}}  >
               <LuFileInput />
               Input
             </Tabs.Trigger>
-            <Tabs.Trigger value="projects">
+            <Tabs.Trigger value='process' _selected={{color:'white'}} >
               <LuFolder />
               Process
             </Tabs.Trigger>
-            <Tabs.Trigger value="tasks">
+            <Tabs.Trigger value='model' _selected={{color:'white'}} >
               <LuUser />
               Model
             </Tabs.Trigger>
-            <Tabs.Indicator rounded="l2"  />
+            <Tabs.Indicator bg='green.700' rounded="l3"  />
           </Tabs.List>
-          <Tabs.Content value="members"><Input /></Tabs.Content>
-          <Tabs.Content value="projects"> <Preprocess /> </Tabs.Content>
-          <Tabs.Content value="tasks">
-            Manage your tasks for freelancers
+          <Tabs.Content value="input"><Input /></Tabs.Content>
+          <Tabs.Content value="process"> <Preprocess /> </Tabs.Content>
+          <Tabs.Content value="model">
+            Disini List Model
           </Tabs.Content>
         </Tabs.Root>
 
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -3,23 +3,28 @@ import React from 'react'
 import {  Button } from '@/components/ui/button'
 import { Flex,Box } from '@chakra-ui/react'
 import { LuCopyPlus } from 'react-icons'
+import { MdBorderColor } from 'react-icons/md'
 
 
 
 
 const Input = () => {
-
   
+  // const variab = {border:'solid 2px', borderColor:'green.300'} // klo mau custom border
 
+  const boxProp = {
+    borderRadius:'lg' ,_hover:{ scale: '1.1',cursor: 'pointer',shadow: 'lg' }, shadow:'sm', border:'solid 1px', borderColor:'teal.100' ,borderRadius:'md'  ,height:'24' ,width:'45%',transition: 'scale 0.1s ease-in'
+  }
+  
 
 
   return (
     <div className='w-full'>
       <Flex gap='3' direction='row' wrap='wrap' justify='space-evenly' align='stretch'>
-        <Box borderRadius='lg' shadow='md' height='20' width='45%'>
+        <Box {...boxProp} >
 
         </Box>
-        <Box borderRadius='lg' shadow='md' height='20' width='45%'>
+        <Box {...boxProp}>
 
         </Box>
       </Flex>
@@ -27,5 +32,8 @@ const Input = () => {
     </div>
   )
 }
+
+
+
 
 export default Input
